d70ecfb4818bb603564851702b9c8889d899c498
Edgar Ortega <edgarortegaramirez@gmail.com>
2020-01-08 11:34:48 -0500

    Remove monokai theme install from scripts/vim.sh
    It is now installed a vim plugin

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -4,11 +4,6 @@ source ./scripts/helpers.sh;
 
 title VIM;
 
-installing "Vim Monokai color theme"
-mkdir -p ${HOME}/.vim/colors
-curl -q -o ${HOME}/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim &> /dev/null
-installed "Vim Monokai color theme"
-
 installing "Vim-plug"
 curl -qfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &> /dev/null
